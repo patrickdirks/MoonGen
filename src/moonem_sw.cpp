16c3e1023139077a97a97ee87406ed792c195043
patrickdirks <patrick.dirks.01@gmail.com>
2026-02-21 17:21:54 +0100

    debug

--- src/moonem_sw.cpp
+++ src/moonem_sw.cpp
@@ -190,7 +190,7 @@ extern "C" void sw_receiver_loop_rate_token_bucket(int port_id, int queue_id, st
 
 extern "C" void sw_receiver_loop_rate_leaky_bucket(int port_id, int queue_id, struct rte_ring* packet_ring, struct moonem_config config){
 	uint64_t delay = rte_get_tsc_hz() * (config.delay / 1000000000.0d);
-	const double MAX_TIME_IN_QUEUE_SECONDS = (config.ce_threshold / 10000000.0d);
+	const double MAX_TIME_IN_QUEUE_SECONDS = (config.ce_threshold / 1000000.0d);
 	const double B_P_TSC_TARGET = config.rate * 1000000.0d / rte_get_tsc_hz();
 	const double BACKLOG_BOUND = config.capacity / (B_P_TSC_TARGET / 8);
 	const double MAX_TIME_IN_QUEUE = rte_get_tsc_hz() * MAX_TIME_IN_QUEUE_SECONDS;
@@ -199,8 +199,7 @@ extern "C" void sw_receiver_loop_rate_leaky_bucket(int port_id, int queue_id, st
 	struct rte_mbuf* bufs_accept[BURST_SIZE];
 	double next_at = 0;
 	uint64_t loss_state = config.loss_seed;
-	printf("delay: %lu, max_time_in_queue: %f, backlog_bound: %f\n", delay, MAX_TIME_IN_QUEUE_SECONDS, BACKLOG_BOUND);
-	while (is_running(0)) {
+	printf("ce_threshold raw: %lu, max_time_in_queue: %f\n", config.ce_threshold, MAX_TIME_IN_QUEUE_SECONDS);	while (is_running(0)) {
 		uint16_t rx = rte_eth_rx_burst(port_id, queue_id, bufs, BURST_SIZE);
 		if(rx > 0){
 			uint64_t recv_timestamp = rte_rdtsc();
